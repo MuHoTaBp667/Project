7fe23a8ad5cef484d07f6bb1eb8faf218daba013
Sergei <sergejgoglacev2@gmail.com>
2026-02-03 13:19:41 +0000

    1

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
-from .models import Phone
 
-def phone_list(request):
-    phones = Phone.objects.all()  # Получаем все записи из таблицы phones
-    return render(request, 'phones/list.html', {'phones': phones})
+
--- myproject/urls.py
+++ myproject/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('phones/', views.phone_list, name='phone_list'),
+
 ]
