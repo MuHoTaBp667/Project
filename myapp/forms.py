76456a4d31caad209dc136dd0126c4bf6df73735
Sergei <sergejgoglacev2@gmail.com>
2026-02-14 11:41:18 +0000

    Добавил плейсхолдеры

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,13 +1,23 @@
 from django import forms
-from django.shortcuts import render, redirect
 
 class RegistrationForm(forms.Form):
-    ID = forms.CharField(max_length=40, label='ID')
-    CallerID = forms.CharField(max_length=40, label='CallerID')
-    Tnumber = forms.CharField(max_length=40, label='Tnumber')
-    ContextRF = forms.ChoiceField (
-        choices=[('internal', 'Intertnal'),('internal_only', 'Internal_only')],
-        widget=forms.Select,
+    ID = forms.CharField(
+        max_length=40, 
+        label='ID',
+        widget=forms.TextInput(attrs={'placeholder': 'Введите ID'})
+    )
+    CallerID = forms.CharField(
+        max_length=40, 
+        label='CallerID',
+        widget=forms.TextInput(attrs={'placeholder': 'Введите CallerID'})
+    )
+    Tnumber = forms.CharField(
+        max_length=40, 
+        label='Tnumber',
+        widget=forms.TextInput(attrs={'placeholder': 'Введите Tnumber'})
+    )
+    ContextRF = forms.ChoiceField(
+        choices=[('internal', 'Internal'), ('internal_only', 'Internal_only')],
+        widget=forms.Select(attrs={'placeholder': 'Выберите контекст'}),
         label='Context'
     )
-    
