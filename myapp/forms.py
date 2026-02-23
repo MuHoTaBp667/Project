84500d318f5fdc200771993b92d336902d91d5f9
Sergei <sergejgoglacev2@gmail.com>
2026-02-23 14:35:16 +0000

    Добавленно редактирование пользователей

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,23 +1,29 @@
 from django import forms
+from .models import PsAuths, PsAors, PsEndpoints
 
-class RegistrationForm(forms.Form):
-    ID = forms.CharField(
-        max_length=40, 
-        label='ID',
-        widget=forms.TextInput(attrs={'placeholder': 'Введите ID'})
-    )
-    CallerID = forms.CharField(
-        max_length=40, 
-        label='CallerID',
-        widget=forms.TextInput(attrs={'placeholder': 'Введите CallerID'})
-    )
-    Tnumber = forms.CharField(
-        max_length=40, 
-        label='Tnumber',
-        widget=forms.TextInput(attrs={'placeholder': 'Введите Tnumber'})
-    )
-    ContextRF = forms.ChoiceField(
-        choices=[('internal', 'Internal'), ('internal_only', 'Internal_only')],
-        widget=forms.Select(attrs={'placeholder': 'Выберите контекст'}),
-        label='Context'
-    )
+CONTEXT_CHOICES = [
+    ('internal', 'Internal'),
+    ('internal_only', 'Internal_only'),
+]
+# создаем выбор для поля context
+
+class AddUserForm(forms.ModelForm):
+
+    class Meta:
+        model = PsEndpoints
+        fields = (
+             "id",
+             "context",
+             "callerid",
+             "tnumber",
+        )
+        # берем поля из бд
+        widgets = {
+            'id':forms.TextInput(attrs={'placeholder': 'Введите ID'}),
+            'callerid':forms.TextInput(attrs={'placeholder': 'Введите CallerID'}),
+            'tnumber':forms.TextInput(attrs={'placeholder': 'Введите Tnumber'}),
+            'context': forms.Select(attrs={'class': 'form-control'},
+                                    choices=CONTEXT_CHOICES)
+        }
+        # настраиваем плейсхолдеры для текста
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,50 +1,65 @@
-from django.shortcuts import render, redirect
-from django.views import View
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import PsAors, PsAuths, PsEndpoints
-from .forms import RegistrationForm
+from .forms import AddUserForm
+from django.db import IntegrityError
 
 def combined_view(request):
-    # получаем данные из 3 таблиц
-    aors_data = PsAors.objects.all().values('id','contact','max_contacts')
+    # Получаем ID для редактирования из GET параметра
+    edit_id = request.GET.get('edit_id')
+    editing_user = None
+    form = AddUserForm()  # Пустая форма по умолчанию
+    
+    # Получаем данные из таблиц
+    aors_data = PsAors.objects.all().values('id', 'contact', 'max_contacts')
     auth_data = PsAuths.objects.all().values('id')
     endpoints_data = PsEndpoints.objects.all().values('id', 'context', 'callerid', 'tnumber', 'transport')
+    
+    # Если есть edit_id - значит режим редактирования
+    if edit_id:
+        try:
+            editing_user = PsEndpoints.objects.get(id=edit_id)
+            form = AddUserForm(instance=editing_user)  # Заполняем форму данными
+        except PsEndpoints.DoesNotExist:
+            messages.error(request, 'Пользователь не найден')
+    
+    # Обработка POST запроса (создание или обновление)
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-             # если запросотправлен методом пост создаем объект класса RegistrationForm
-        if form.is_valid():
-            id_value = form.cleaned_data['ID']
-            callerid_value = form.cleaned_data['CallerID']
-            tnumber_value = form.cleaned_data['Tnumber']
-            context_value = form.cleaned_data['ContextRF']
-            # если форма "валидна" получаем очищенные данные
-            PsAors.objects.create(
-                id=id_value,
-                max_contacts=2
-            )
-            PsAuths.objects.create(id=id_value)
-
-            PsEndpoints.objects.create(
-                id=id_value,
-                context=context_value,
-                callerid=callerid_value,
-                tnumber=tnumber_value,
-                transport='transport-udp',
-                disallow='all', 
-                allow='alaw,ulaw',  
-                direct_media='yes')
-            # добавляем записи в таблицы
-            return redirect('combined_view')
-            # перенаправляем пользователя после заполнения формы
-    else:
-        form = RegistrationForm()
-
-
+        # Проверяем, это создание или обновление
+        user_id = request.POST.get('id')
+        
+        if user_id and PsEndpoints.objects.filter(id=user_id).exists():
+            # РЕЖИМ РЕДАКТИРОВАНИЯ - обновляем существующую запись
+            endpoint = PsEndpoints.objects.get(id=user_id)
+            form = AddUserForm(request.POST, instance=endpoint)
+            
+            if form.is_valid():
+                form.save()
+                messages.success(request, f'Пользователь {user_id} успешно обновлен!')
+                return redirect('combined_view')
+        else:
+            # РЕЖИМ СОЗДАНИЯ - новая запись
+            form = AddUserForm(request.POST)
+            if form.is_valid():
+                id_value = form.cleaned_data['id']
+                
+                try:
+                    # Создаём записи во всех таблицах
+                    PsAors.objects.create(id=id_value, contact='', max_contacts=2)
+                    PsAuths.objects.create(id=id_value)
+                    form.save()  # Сохраняем PsEndpoints
+                    
+                    messages.success(request, 'Пользователь успешно создан!')
+                    return redirect('combined_view')
+                except IntegrityError as e:
+                    messages.error(request, f'Ошибка: {e}')
+    
     context = {
         'aors': aors_data,
         'auth': auth_data,
         'endpoints_data': endpoints_data,
         'form': form,
+        'edit_mode': bool(edit_id),  # True если редактируем
+        'editing_user': editing_user,
     }
     return render(request, 'combined_view.html', context)
-    # берем шаблон страницы подставляем в него данные из контекста возвращаем это обратно уже с данными
